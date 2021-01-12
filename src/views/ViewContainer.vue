2ecbab45e96292271e38138a3d8b4a72ac0d70d0
Franco <franco@localhost.localdomain>
2021-01-12 11:25:47 -0300

    ViewContainer added

---
+++ src/views/ViewContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class=" w-screen h-screen bg-gray-200 ">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewContainer",
+};
+</script>
+
+<style></style>
